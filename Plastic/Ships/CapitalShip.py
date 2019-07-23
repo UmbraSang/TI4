59c8a529fc45a1e10b411dc73d190cce1ff1a1da
Connor <36247578+UmbraSang@users.noreply.github.com>
2019-07-23 16:30:08 -0700

    Add files via upload

--- Plastic/Ships/CapitalShip.py
+++ Plastic/Ships/CapitalShip.py
@@ -7,3 +7,10 @@ class CapitalShip(Unit):
     def __init__(self):
         Unit.__init__(self)
 
+    def bombardPlanet(self):
+        if self.canBombard:
+            for x in range(self.numDice):
+                # choose target
+                target = 0
+                if self.roll() >= self.hit:
+                    self.location.planets[target].receiveBombard()
--- Plastic/Ships/Dreadnought.py
+++ Plastic/Ships/Dreadnought.py
@@ -15,3 +15,5 @@ class Dreadnought(SustainClass):
 
         self.canBombard = True
         #
+
+
